f8700540d9ad96c400303b1fca4cfe3ee3b73151
harininishant <harini.nishant@gmail.com>
2021-08-31 20:02:46 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int populationofmissisauga = 700000;
+	int amount;
+	amount = 20;
+	int num1,num2,num3;
+	int num4 = 10 , num5 = 30;
+
+	int sizeofclass =2147483647;
+	short sizeofgroup =5;
+	long populationofworld = 700000000;
+        System.out.println(sizeofclass);
+        System.out.println(sizeofgroup);
+        System.out.println(populationofworld);
+
+        float amount1 = 3.4567F;
+        float pi = 3.14f;
+        double balance = 10.345678908356;
+        double balance1 = 11.2345768;
+		System.out.println(amount1);
+		System.out.println(pi);
+		System.out.println(balance);
+		System.out.println(balance1);
+
+		boolean iscanadian = false;
+		char name = 'h';
+		char anothername = 82;
+		System.out.println(iscanadian);
+		System.out.println(name);
+		System.out.println(anothername);
+		//byte is given 1 byte to store
+		byte data = 127;
+
+
+
+    }
+}
